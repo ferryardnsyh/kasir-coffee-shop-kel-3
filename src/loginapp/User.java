ca5ed9554291c2616a1462ca7ad23939353a1e55
LAPTOP-VCKS26TI\naufal <naufalmuflihmuzakki@gmail.com>
2025-12-08 15:36:34 +0700

    menambahkan lgin

---
+++ src/loginapp/User.java
@@ -0,0 +1,20 @@
+package loginapp;
+
+public class User {
+    private final String username = "admin";
+    private final String password = "123";
+
+    public boolean login(String user, String pass) {
+        return username.equals(user) && password.equals(pass);
+    }
+}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author naufal
+ */
+
